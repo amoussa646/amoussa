7c5eeaf5953cd6bcb760bbe915c07d4cbd8917cf
opeth646 <40394822+opeth646@users.noreply.github.com>
2024-06-12 00:21:11 +0300

    kk

---
+++ src/data/aboutData.js
@@ -0,0 +1,8 @@
+export const aboutData = {
+  title: "Who I am",
+  description1:
+    "I began studying Mechatronics engineering to build robots, quickly excelling in mechanics and electronics. Realizing the need for advanced programming to make my creations smart and useful, I switched to Computer Engineering.",
+  description2:
+    "My primary focus is machine learning due to its pivotal role in robotics. Along the way, I became proficient in Flutter for app development and FastAPI for backend development due to always depending on them in my personal projects and my last work experience.",
+  image: 2,
+};
--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,31 @@
-const TerserPlugin = require('terser-webpack-plugin');
+const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = {
   entry: `${__dirname}/src/index.tsx`,
   output: {
     path: `${__dirname}/build`,
-    publicPath: '/build/',
-    filename: 'bundle.js',
+    publicPath: "amoussa/build/",
+    filename: "bundle.js",
   },
 
   // generate different source maps for dev and production
-  devtool: process.argv.indexOf('-p') === -1 ? 'eval-source-map' : 'source-map',
+  devtool: process.argv.indexOf("-p") === -1 ? "eval-source-map" : "source-map",
 
   resolve: {
-    extensions: ['.ts', '.tsx', '.js'],
+    extensions: [".ts", ".tsx", ".js"],
   },
 
   module: {
     rules: [
       // use ts-loader for ts and js files so all files are converted to es5
-      { test: /\.(tsx?|js)$/, exclude: /node_modules/, loader: 'ts-loader' },
-      { test: /\.js$/, loader: 'source-map-loader' },
+      { test: /\.(tsx?|js)$/, exclude: /node_modules/, loader: "ts-loader" },
+      { test: /\.js$/, loader: "source-map-loader" },
     ],
   },
 
   // required because the defaults for webpack -p don't remove multiline comments
   optimization:
-    process.argv.indexOf('-p') === -1
+    process.argv.indexOf("-p") === -1
       ? {}
       : {
           minimize: true,
@@ -45,7 +45,7 @@ module.exports = {
   // and test spa-github-pages redirect in dev
   devServer: {
     historyApiFallback: {
-      rewrites: [{ from: /\//, to: '/404.html' }],
+      rewrites: [{ from: /\/amoussa\/[^?]/, to: "/404.html" }],
     },
   },
 };
